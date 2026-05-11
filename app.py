a641acd029c8f1cb886e5eb851f7376a0e5c6aa6
RushikeshDahale <rushikeshathitech@gmail.com>
2026-05-11 08:30:06 +0530

    fixed render deployment

--- app.py
+++ app.py
@@ -269,4 +269,7 @@ def pending(id):
 
 init_db()
 
-app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
